7e0c4d589c292311bfba4e69df781c0c3d55ea3b
vo-vuong <vovuong1025@gmail.com>
2021-11-24 20:13:27 +0700

    how to use async action on component

--- src/features/Auth/components/Register/index.jsx
+++ src/features/Auth/components/Register/index.jsx
@@ -1,12 +1,25 @@
+import { unwrapResult } from '@reduxjs/toolkit';
+import { register } from 'features/Auth/userSlice';
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import RegisterForm from '../RegisterForm';
 
 Register.propTypes = {};
 
 function Register(props) {
-  const handleRegisterFormSubmit = (values) => {
-    console.log('Lam gi do sau khi form submit');
-    console.log('Form Register Submit:', values);
+  const dispatch = useDispatch();
+
+  const handleRegisterFormSubmit = async (values) => {
+    // console.log('Lam gi do sau khi form submit');
+    // console.log('Form Register Submit:', values);
+    try {
+      const action = register(values); // values la truy vao cai payload ben userSlice
+      const resultAction = await dispatch(action); // gọi api đang ki ben userSlice xong doi no
+      const user = unwrapResult(resultAction);
+      console.log('New user', user);
+    } catch (error) {
+      console.log('Failed to register:', error);
+    }
   };
 
   return (
