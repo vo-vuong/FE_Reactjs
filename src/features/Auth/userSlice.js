e0d38a7ce303b43d928326cbb25868c16489f6d3
vo-vuong <vovuong1025@gmail.com>
2021-11-25 22:53:20 +0700

    fix response from API is user and init redux form localStorage

--- src/features/Auth/userSlice.js
+++ src/features/Auth/userSlice.js
@@ -8,10 +8,10 @@ export const register = createAsyncThunk('users/register', async (payload) => {
   const data = await userApi.register(payload);
   // save data to local storage
   // localStorage.setItem('token', data.jwt);
-  localStorage.setItem(StorageKeys.USER, JSON.stringify(data.object));
+  localStorage.setItem(StorageKeys.USER, JSON.stringify(data.user));
   // data tra ra khi request to server
   // console.log(data);
-  return data.object; //cho nay return ra payload cho action*  Vi vay cho nay return ra user data
+  return data.user; //cho nay return ra payload cho action*  Vi vay cho nay return ra user data
 });
 
 export const login = createAsyncThunk('users/login', async (payload) => {
@@ -19,15 +19,15 @@ export const login = createAsyncThunk('users/login', async (payload) => {
   const data = await userApi.login(payload);
   // save data to local storage
   localStorage.setItem(StorageKeys.TOKEN, data.token);
-  localStorage.setItem(StorageKeys.USER, JSON.stringify(data.object));
+  localStorage.setItem(StorageKeys.USER, JSON.stringify(data.user));
 
-  return data.object; //cho nay return ra payload cho action*  Vi vay cho nay return ra user data
+  return data.user; //cho nay return ra payload cho action*  Vi vay cho nay return ra user data
 });
 
 const userSlice = createSlice({
   name: 'user',
   initialState: {
-    current: {},
+    current: JSON.parse(localStorage.getItem(StorageKeys.USER)) || {}, // O day set gia tri State luc khoi tao
     setting: {},
   },
   reducers: {},
