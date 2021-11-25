15267cc87ee75a5d6993f552927e5a36ec407c8b
vo-vuong <vovuong1025@gmail.com>
2021-11-25 22:38:32 +0700

    add constant LocalStorage

--- src/features/Auth/userSlice.js
+++ src/features/Auth/userSlice.js
@@ -1,5 +1,6 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import userApi from 'api/userApi';
+import StorageKeys from 'constants/storage-keys';
 
 export const register = createAsyncThunk('users/register', async (payload) => {
   // neu can dispatch mot action khac thi sai thunkAPI. thunkAPI them o phan tham so
@@ -7,7 +8,7 @@ export const register = createAsyncThunk('users/register', async (payload) => {
   const data = await userApi.register(payload);
   // save data to local storage
   // localStorage.setItem('token', data.jwt);
-  localStorage.setItem('user', JSON.stringify(data.object));
+  localStorage.setItem(StorageKeys.USER, JSON.stringify(data.object));
   // data tra ra khi request to server
   // console.log(data);
   return data.object; //cho nay return ra payload cho action*  Vi vay cho nay return ra user data
@@ -17,8 +18,8 @@ export const login = createAsyncThunk('users/login', async (payload) => {
   // neu can dispatch mot action khac thi sai thunkAPI. thunkAPI them o phan tham so
   const data = await userApi.login(payload);
   // save data to local storage
-  localStorage.setItem('token', data.token);
-  localStorage.setItem('user', JSON.stringify(data.object));
+  localStorage.setItem(StorageKeys.TOKEN, data.token);
+  localStorage.setItem(StorageKeys.USER, JSON.stringify(data.object));
 
   return data.object; //cho nay return ra payload cho action*  Vi vay cho nay return ra user data
 });
