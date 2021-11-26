07a6fdaaceaedc532f9eb516ed0cdba318521fbc
vo-vuong <vovuong1025@gmail.com>
2021-11-26 20:10:46 +0700

    add routing page products

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import ProductFeature from 'features/Product';
+import { Route, Switch } from 'react-router';
 import './App.css';
 import Header from './components/Header';
 
@@ -5,8 +7,14 @@ function App() {
   return (
     <div className="App">
       <Header />
+
+      <Switch>
+        <Route path="/products" component={ProductFeature} />
+      </Switch>
     </div>
   );
 }
 
 export default App;
+
+// Trong ProductFeature lai co routing de di xuong trang listing.
