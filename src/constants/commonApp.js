662d3c0ee1c65fd2618d4a45a7b75ff8cae44383
vo-vuong <vovuong1025@gmail.com>
2021-11-27 16:40:25 +0700

    handle image product

---
+++ src/constants/commonApp.js
@@ -0,0 +1,1 @@
+export const STATIC_IMAGE = 'http://localhost:3000/images/360.png';
--- src/features/Product/components/Product.jsx
+++ src/features/Product/components/Product.jsx
@@ -1,18 +1,21 @@
-import React from 'react';
-import PropTypes from 'prop-types';
 import { Box, Typography } from '@material-ui/core';
-import { Skeleton } from '@material-ui/lab';
+import { STATIC_IMAGE } from 'constants/index';
+import PropTypes from 'prop-types';
+import React from 'react';
 
 Product.propTypes = {
   product: PropTypes.object,
 };
 
 function Product({ product }) {
-  //   console.log(product);
+  // console.log(product.images[0].url);
+  const thumbnailUrl = product.images.length > 0 ? product.images[0]?.url : STATIC_IMAGE; // dang nho
+
   return (
     <Box padding={1}>
-      <Skeleton variant="rect" width="100%" height={118} />
-
+      <Box padding={1}>
+        <img src={thumbnailUrl} alt={product.name} width="100%"></img>
+      </Box>
       <Typography variant="body2">{product.name}</Typography>
       <Typography variant="body2">{product.price}</Typography>
     </Box>
