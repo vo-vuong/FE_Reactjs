c3bcf09484bbfa5fb861169ce26bec6e25f8377d
vo-vuong <vovuong1025@gmail.com>
2021-11-29 21:04:27 +0700

    custom fetch data product

--- src/features/Product/components/Product.jsx
+++ src/features/Product/components/Product.jsx
@@ -2,6 +2,7 @@ import { Box, Typography } from '@material-ui/core';
 import { STATIC_IMAGE } from 'constants/index';
 import PropTypes from 'prop-types';
 import React from 'react';
+import { useHistory } from 'react-router';
 
 Product.propTypes = {
   product: PropTypes.object,
@@ -10,9 +11,14 @@ Product.propTypes = {
 function Product({ product }) {
   // console.log(product.images[0].url);
   const thumbnailUrl = product.images.length > 0 ? product.images[0]?.url : STATIC_IMAGE; // dang nho
+  const history = useHistory();
 
+  const handleClick = () => {
+    // Navigate to detail page: /products/:productId
+    history.push(`/products/${product.id}`);
+  };
   return (
-    <Box padding={1}>
+    <Box padding={1} onClick={handleClick}>
       <Box padding={1} minHeight="215px">
         <img src={thumbnailUrl} alt={product.name} width="100%"></img>
       </Box>
--- src/features/Product/pages/DetailPage.jsx
+++ src/features/Product/pages/DetailPage.jsx
@@ -1,6 +1,8 @@
 import { Box, Container, Grid, makeStyles, Paper } from '@material-ui/core';
 import React from 'react';
+import { useRouteMatch } from 'react-router';
 import ProductThumbnail from '../components/ProductThumbnail';
+import useProductDetail from '../hooks/useProductDetail';
 
 const useStyles = makeStyles((theme) => ({
   root: {},
@@ -18,6 +20,19 @@ const useStyles = makeStyles((theme) => ({
 
 function DetailPage() {
   const classes = useStyles();
+  // const match = useRouteMatch();
+  // console.log({ match }); get param do tren url
+  const {
+    params: { productId },
+  } = useRouteMatch(); // day la destructuring 2 tang
+  //Binh thuong khi co productId roi se useEffect get san pham. nhung bay gio se lam custom hook
+
+  const { product, loading } = useProductDetail(productId);
+
+  if (loading) {
+    // Tao cai loading cho dep ti
+    return <Box>Loading</Box>;
+  }
 
   return (
     <Box className={classes.root}>
@@ -25,7 +40,7 @@ function DetailPage() {
         <Paper elevation={0}>
           <Grid container>
             <Grid item className={classes.left}>
-              <ProductThumbnail product={{}} />
+              <ProductThumbnail product={product} />
             </Grid>
             <Grid item className={classes.right}>
               Product Info
