8180f6f54c6175ffc39e0f34e18cc51333876302
vo-vuong <vovuong1025@gmail.com>
2021-12-10 00:51:33 +0700

    fix delete default timestamp when test

--- src/utils/date.js
+++ src/utils/date.js
@@ -1,7 +1,6 @@
 import { format } from 'date-fns';
 
 export function formatDateTime(timestamp) {
-  var timestamp = 1607110465663;
   var date = new Date(timestamp);
 
   return format(new Date(date), 'dd MMM yyyy HH:mm');
