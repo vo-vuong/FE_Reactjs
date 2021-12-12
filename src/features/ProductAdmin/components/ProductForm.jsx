57b0bd046d62a3e5582d1345a9aa04dae5bc78fd
vo-vuong <vovuong1025@gmail.com>
2021-12-12 18:42:22 +0700

    add template select category id

--- src/features/ProductAdmin/components/ProductForm.jsx
+++ src/features/ProductAdmin/components/ProductForm.jsx
@@ -8,12 +8,13 @@ import {
   makeStyles,
   Radio,
   RadioGroup,
+  TextField,
 } from '@material-ui/core';
 import InputField from 'components/form-controls/InputField';
 import InputNumberField from 'components/form-controls/InputNumberField';
 import MultilineField from 'components/form-controls/MultilineField';
 import PropTypes from 'prop-types';
-import { React } from 'react';
+import { React, useState } from 'react';
 import { Controller, useForm } from 'react-hook-form';
 import { useHistory } from 'react-router-dom';
 import * as yup from 'yup';
@@ -21,6 +22,25 @@ import * as yup from 'yup';
 ProductForm.propTypes = {
   onSubmit: PropTypes.func,
 };
+
+const currencies = [
+  {
+    value: 'USD',
+    label: '$',
+  },
+  {
+    value: 'EUR',
+    label: '€',
+  },
+  {
+    value: 'BTC',
+    label: '฿',
+  },
+  {
+    value: 'JPY',
+    label: '¥',
+  },
+];
 
 const useStyles = makeStyles((theme) => ({
   root: { flexGrow: 1, marginLeft: '30px', marginTop: '40px', marginRight: '30px' },
@@ -40,33 +60,34 @@ const useStyles = makeStyles((theme) => ({
 function ProductForm(props) {
   const classes = useStyles();
   const history = useHistory();
+  const [currency, setCurrency] = useState('EUR');
 
   const schema = yup.object({
-    name: yup
-      .string()
-      .required('Vui lòng nhập Tên sản phẩm.')
-      .min(3, 'Vui lòng nhập Tên sản phẩm lớn hơn 3 kí tự.')
-      .max(255, 'Vui lòng nhập Tên sản phẩm nhỏ hơn 255 kí tự.'),
-    shortdescription: yup
-      .string()
-      .required('Vui lòng nhập Mô tả ngắn')
-      .min(20, 'Vui lòng nhập Mô tả ngắn lớn hơn 20 kí tự.')
-      .max(255, 'Vui lòng nhập Mô tả ngắn nhỏ hơn 255 kí tự.'),
-    detail: yup
-      .string()
-      .required('Vui lòng nhập Mô tả chi tiết')
-      .min(20, 'Vui lòng nhập Mô tả chi tiết lớn hơn 20 kí tự.'),
-    price: yup.number().required().positive().integer(),
-    originId: yup.number().required().positive().integer(),
-    quantity: yup.number().required().positive().integer(),
-    code: yup
-      .string()
-      .required('Vui lòng nhập Code.')
-      .min(5, 'Vui lòng nhập Code lớn hơn 5 kí tự.')
-      .max(255, 'Vui lòng nhập Code nhỏ hơn 255 kí tự.'),
+    // name: yup
+    //   .string()
+    //   .required('Vui lòng nhập Tên sản phẩm.')
+    //   .min(3, 'Vui lòng nhập Tên sản phẩm lớn hơn 3 kí tự.')
+    //   .max(255, 'Vui lòng nhập Tên sản phẩm nhỏ hơn 255 kí tự.'),
+    // shortdescription: yup
+    //   .string()
+    //   .required('Vui lòng nhập Mô tả ngắn')
+    //   .min(20, 'Vui lòng nhập Mô tả ngắn lớn hơn 20 kí tự.')
+    //   .max(255, 'Vui lòng nhập Mô tả ngắn nhỏ hơn 255 kí tự.'),
+    // detail: yup
+    //   .string()
+    //   .required('Vui lòng nhập Mô tả chi tiết')
+    //   .min(20, 'Vui lòng nhập Mô tả chi tiết lớn hơn 20 kí tự.'),
+    // price: yup.number().required().positive().integer(),
+    // originId: yup.number().required().positive().integer(),
+    // quantity: yup.number().required().positive().integer(),
+    // code: yup
+    //   .string()
+    //   .required('Vui lòng nhập Code.')
+    //   .min(5, 'Vui lòng nhập Code lớn hơn 5 kí tự.')
+    //   .max(255, 'Vui lòng nhập Code nhỏ hơn 255 kí tự.'),
     // status: yup.number().required().positive().integer(),
-    categoryId: yup.number().required('Vui lòng nhập Danh mục sản phẩm.').positive().integer(),
-    warranty: yup.number().required('Vui lòng nhập Bảo hành sản phẩm.').positive().integer(),
+    // categoryId: yup.number().required('Vui lòng nhập Danh mục sản phẩm.').positive().integer(),
+    // warranty: yup.number().required('Vui lòng nhập Bảo hành sản phẩm.').positive().integer(),
   });
 
   const form = useForm({
@@ -79,7 +100,7 @@ function ProductForm(props) {
       quantity: '',
       code: '',
       status: '1',
-      categoryId: '',
+      // categoryId: '',
       warranty: '',
     },
 
@@ -90,11 +111,19 @@ function ProductForm(props) {
     history.push('/admin/product');
   };
 
+  const handleChange = (event) => {
+    setCurrency(event.target.value);
+  };
+
   const handleSubmit = async (values) => {
-    console.log(values);
+    const object2 = {
+      ...values,
+      categoryId: currency,
+    };
+    console.log(object2);
     const { onSubmit } = props;
     if (onSubmit) {
-      await onSubmit(values);
+      await onSubmit(object2);
     }
 
     // form.reset();  Bo vi khi loi thi khong reset form  ma khi thanh cong thi dong form roi
@@ -123,7 +152,25 @@ function ProductForm(props) {
           }
         />
       </FormControl>
-      <InputField name="categoryId" label="Danh mục sản phẩm*" form={form} />
+      {/* <InputField name="categoryId" label="Danh mục sản phẩm*" form={form} /> */}
+      <TextField
+        id="outlined-select-currency-native"
+        select
+        label="Native select"
+        fullWidth
+        value={currency}
+        onChange={handleChange}
+        SelectProps={{
+          native: true,
+        }}
+        variant="outlined"
+      >
+        {currencies.map((option) => (
+          <option key={option.value} value={option.value}>
+            {option.label}
+          </option>
+        ))}
+      </TextField>
       <InputNumberField name="warranty" label="Bảo hành*" size="small" form={form} />
 
       <Box className={classes.boxButton}>
