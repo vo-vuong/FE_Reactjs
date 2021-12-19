5c796e97ff49c723c5c77fbec166cc30b3159d58
vo-vuong <vovuong1025@gmail.com>
2021-12-19 18:37:19 +0700

    add favorite content Home page

--- src/features/Home/pages/HomePage.jsx
+++ src/features/Home/pages/HomePage.jsx
@@ -1,13 +1,22 @@
 import { Container, Grid } from '@material-ui/core';
+import contentApi from 'api/contentApi';
 import productApi from 'api/productApi';
 import ImageBanner from 'components/ImageBanner';
 import React, { useEffect, useState } from 'react';
+import FavoriteContentList from '../components/FavoriteContentList';
 import FavoriteProductList from '../components/FavoriteProductList';
 
 function HomePage() {
   const ImgBannerProductStyle = { width: '100%', height: '50vh' };
-  const ImgBannerContentStyle = { width: '100%', height: '200px', marginTop: '20px' , backgroundImage: 'url(https://images.pexels.com/photos/7520779/pexels-photo-7520779.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)'};
+  const ImgBannerContentStyle = {
+    width: '100%',
+    height: '200px',
+    marginTop: '20px',
+    backgroundImage:
+      'url(https://images.pexels.com/photos/7520779/pexels-photo-7520779.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)',
+  };
   const [productList, setProductList] = useState([]);
+  const [contentList, setContentList] = useState([]);
 
   useEffect(() => {
     const filters = {
@@ -16,8 +25,7 @@ function HomePage() {
     };
     (async () => {
       try {
-        // goi request len server thi kem try catch
-        const { data } = await productApi.getAll({ filters }); // desturing data
+        const { data } = await productApi.getAll({ filters });
         setProductList(data.slice(0, 8));
       } catch (error) {
         console.log('Failed load product', error);
@@ -27,25 +35,43 @@ function HomePage() {
     })();
   }, []);
 
+  useEffect(() => {
+    const filters = {
+      page: 1,
+      sort: 'moi-cu',
+    };
+    (async () => {
+      try {
+        const { data } = await contentApi.getAll({ filters });
+        setContentList(data.slice(0, 4));
+      } catch (error) {
+        console.log('Failed load content', error);
+      }
+    })();
+  }, []);
+
   return (
-    <Container>
+    <Container style={{ marginBottom: '20px' }}>
       <ImageBanner
-        title="Hello Xin chào!!!"
-        description="HI"
-        linkText="Đi đâu đó"
-        link="/hi"
+        title="Chuyên cung cấp nhạc cụ."
+        description="Đặt hàng online - Thanh toán khi nhận hàng."
+        // linkText="Đi đâu đó"
+        // link="/hi"
         style={ImgBannerProductStyle}
       />
       <Grid container>
         <FavoriteProductList title="Sản phẩm nổi bật" productList={productList} />
       </Grid>
       <ImageBanner
-        title="Hello Bai viet!!!"
-        description="HI"
-        linkText="Đi đâu đó"
-        link="/hi"
+        title="Thế giới của âm nhạc"
+        description="Bạn chưa biết chọn loại nhạc cụ nào?"
+        // linkText="Đi đâu đó"
+        // link="/hi"
         style={ImgBannerContentStyle}
       />
+      <Grid container>
+        <FavoriteContentList title="Bài viết nổi bật" contentList={contentList} />
+      </Grid>
     </Container>
   );
 }
